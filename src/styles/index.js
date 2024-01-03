41cbd56cc0136bd3e4a5020009218c6d5269e29f
Johanh0 <johanherrera@Johans-MBP.lan>
2024-01-03 12:58:51 -0500

    New components added and also new section for he hero added

---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+const styles = {
+  padding: "w-full px-24 py-14",
+
+  heading_xl: "text-heading_xl font-bold leading-110 -tracking-3.2",
+};
+
+export default styles;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#345FF6",
+        secondary: "#D6E7FA",
+      },
+      fontSize: {
+        heading_xl: "64px",
+        heading_l: "48px",
+        heading_m: "24px",
+        heading_s: "20px",
+        body_m: "16px",
+        body_s: "14px",
+      },
+
+      lineHeight: {
+        110: "110%",
+      },
+      letterSpacing: {
+        "-3.2": "-3.2px",
+      },
+    },
+  },
+  plugins: [],
+};
